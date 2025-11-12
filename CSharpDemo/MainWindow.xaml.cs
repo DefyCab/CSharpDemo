2ac7c41064f60059c69128ed80205a9db04b13e6
Love Bellinder <love.bellinder@gmail.com>
2025-11-12 13:51:16 +0100

    Add info about arrays.

--- CSharpDemo/MainWindow.xaml.cs
+++ CSharpDemo/MainWindow.xaml.cs
@@ -28,15 +28,25 @@ namespace CSharpDemo
             bool sant_eller_falskt = true;
 
             // ARRAY
+
             // Om du vill lagra fler än en variabel använder du en array.
             int[] nummer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
             double[] decimalnummer = [1.1, 2.2, 3.4, 5.4];
             string[] personer = ["Love", "Tom", "Sven", "Kurt", "Bilal", "Natahlie"];
             bool[] check = [true, false, true, false];
 
-            // om du vill ha en tom array med 10 platser så skriver du
-            int[] emptyNumbersArray = new int[10];
-            string[] emptyStringArray = new string[10];
+            // Om du vill ha en tom array med specificerat antal platser så skriver du
+            int[] emptyNumbersArray = new int[10]; // en tom array med tio lagringsplatser.
+            string[] emptyStringArray = new string[5];
+
+            // Arrays är indexerade. Det första värdet har index 0.
+            // Vill du komma åt 1:an i arrayen nummer så kan du skriva nummer[0]
+
+            // lagra ett nummer från en array i ny variabel
+
+            int ny = nummer[0]; // ny får värdet 1 (index 0 motsvarar första värdet i arrayen)
+            int ny2 = nummer[2]; // nu får värdet 3 (index 2 motsvarar tredje värdet i arrayen)
+
 
         }
 
