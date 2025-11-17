0f757610ae909344b62906c8e4eb34d476fc1535
Love Bellinder <love.bellinder@gmail.com>
2025-11-17 11:27:06 +0100

    Void methods.

--- CSharpDemo/MainWindow.xaml.cs
+++ CSharpDemo/MainWindow.xaml.cs
@@ -61,30 +61,114 @@ namespace CSharpDemo
             {
                 nummerArray[i] = treNummer[i];
             }
-
-            // Varv 1 
-            // i = 0;
-            // nummerArray[0] = nioNummer[0] (dvs 1, det första värdet i arrayen)
-
-            // Varv 2
-            // i = 1
-            // nummerarray[1] = nioNummer[1] (dvs 2, det andra värdet i arrayen)
-
-            // Varv 3
-            // i = 2
-            // nummerarray[2] = nioNummer[2] (dvs 3, det tredje värdet i arrayen)
-
-            // Varv 4
-            // i = 3
-            // Loopen stannar då i nu är större än nummerArryas längd som är 3.
-
-
         }
 
+        // Varv 1 
+        // i = 0;
+        // nummerArray[0] = nioNummer[0] (dvs 1, det första värdet i arrayen)
 
+        // Varv 2
+        // i = 1
+        // nummerarray[1] = nioNummer[1] (dvs 2, det andra värdet i arrayen)
 
+        // Varv 3
+        // i = 2
+        // nummerarray[2] = nioNummer[2] (dvs 3, det tredje värdet i arrayen)
 
+        // Varv 4
+        // i = 3
+        // Loopen stannar då i nu är större än nummerArryas längd som är 3.
 
+        // DUBBLA LOOPAR
+
+        // Man kan även ha en dubbel for-loop för att jämföra två arrayer t.ex
+        // Här har jag lagt loopen i ett clcikevent som är kopplad till en knapp i gränssnittet
+        // dvs det som användaren ser, den gröna stora knappen.
+
+        // Med den dubbla for-loopen är det så fiffigt att i stannar på värdet 0
+        // när programemt går vidare in i j-loopen. Då ökar bara j med 1 och i är hela
+        // tiden 0. Då kan man t.ex jämföra arrayer som här
+
+        // VARV 1
+        // i = 0 (värde "Volvo")
+        // j = 0 (värde "Opel")
+
+        // Varv 2
+        // i = 0 (värde "Vovlo")
+        // j = 1 (värde "Saab"
+
+        // Först när alla sex bilar i jonathansbilar gåtts igenom så ökar i till 1
+        // och sen börjar det om igen fast med i=1, j=0, i=1, j=1, i=1, j=2. osv 
+
+        private void ClickToCompareCars(object sender, RoutedEventArgs e)
+        {
+            //string[] eriksBilar = ["Volvo", "Bmv", "Fiat", "Opel"];
+            //string[] jonathansBilar = ["Opel", "Saab", "Porsche", "Lada", "Mazda"];
+
+            //for (int i = 0; i < eriksBilar.Length; i++)
+            //{
+            //    for (int j = 0; j < jonathansBilar.Length; j++)
+            //    {
+            //        if (eriksBilar[i] == jonathansBilar[j])
+            //        {
+            //            MessageBox.Show($"Jaså du har också en {jonathansBilar[j]}");
+            //        }
+            //    }
+            //}
+            //SayAName();
+            //SayAName();
+            //SayAName();
+            //SayAName();
+            //SayAName();
+            //string greeting = WriteAName();
+            //textBox.Text = greeting;
+            test();
+        }
+
+        // Istället för att lägga all kod i clickeventet ovanför så kan du skriva en egeen kod och aktivera 
+        // genom att kalla metoden. På rad 102 kallar jag på metoden SayAName(). Då körs koden som 
+        // finns inom SayANames scope, markerat med vita linjer. (OKEJ LÅDAN!). Test kör programmet
+        // och klicka på "KNAPP".
+
+        private void SayAName()
+        {
+            MessageBox.Show("LOVE!");
+        }
+
+        // Du kan också retuerna variablar från metoder. Här skickar jag med strängen hello.
+        // Som du ser på rad 103 och 104 så kan jag lagra det som metoden returnerar i en variabel
+        // och sen visa upp den som här eller göra vad jag vill med den.
+        // ska du returnera något måste du byta ut "void" före metodens namn mot datatypen du vill
+        // skicka med se "private string" här nedanför.
+
+        private string WriteAName()
+        {
+            string hello = "Love säger hallå";
+            return hello;
+        }
+
+        //bool isValidNumber()
+        //{
+        //    int tru = 10;
+        //    bool test = true;
+        //    if (tru < 3)
+        //    {
+        //        test = false;
+        //    }
+        //    return tru;
+        //}
+
+        private void test()
+        {
+            int test2;
+            int test3;
+            bool res;
+            int a;
+            string myStr = "120";
+            res = int.TryParse(myStr, out a);
+            test2 = a;
+            MessageBox.Show("String is a numeric representation: " + res);
+        }
 
     }
 }
---
+++ Metoder/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Metoder
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            SumOfTwoNumbers();
+            MessageBox.Show($"{_sum}");
+        }
+
+        // OLIKA METODER
+
+        // VOID
+
+        // Här använder jag en metod som är void. Den skickar inte tillbaka något värde
+        // men har tillgång till fält-variablar. Jag kallar metoden på rad 24 så när jag
+        // klickar på knappen "BUTTON" räknas summan ut av _a + _b och visas i messagebox.
+
+        int _sum = 0;
+        int _a = 10;
+        int _b = 5;
+
+        private void SumOfTwoNumbers()
+        {
+            _sum = _a + _b;
+        }
+
+        // RETURN METHODS
+    }
+}
